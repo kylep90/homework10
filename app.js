dca6007c6b93d528b59a6e313d945571af40a1d6
kylep90 <61998043+kylep90@users.noreply.github.com>
2020-06-20 21:47:15 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -10,9 +10,149 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const all = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function addManager (){
+    console.log("Start your team");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message:"What is the manager's id?"
+        },
+        {
+            type:"input",
+            name: "managerEmail",
+            message: "What is the manager's email address?"
+        },
+        {
+            type: "input",
+            name: "managerDept",
+            message: "For which department are they manager for? "
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is the manager's Office Number?"
+        }
+    ]).then(response => {
+        const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerEmail, response.managerDept, response.managerOfficeNumber);
+        all.push(manager);
+        addMembers();
+      
+    });
+}
+
+function addMembers(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "teamType",
+            message: "Which type of member would you like to add?",
+            choices: [
+                "Another Manager",
+                "Engineer", 
+                "Intern",
+                 "No more members" //ADD EXTRA MANAGER
+            ]
+        }
+    ]).then(memberType => {
+        if(memberType.teamType === "Another Manager"){
+            addManager();
+        }
+        else if(memberType.teamType === "Engineer"){
+            createEngineer();
+        }
+        else if(memberType.teamType == "Intern"){
+            createIntern();
+        }
+        else {
+            buildTeam();
+        }
+
+    })
+}
+
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message:"What is this engineer's id?"
+        },
+        {
+            type:"input",
+            name: "engineerEmail",
+            message: "What is this engineer's email address?"
+        },
+        {
+            type: "input",
+            name: "engineerDept",
+            message: "For which department do they work for?"
+        },
+        {
+            type: "input",
+            name: "engineerGit",
+            message: "What is their GitHub username? "
+        }
+    ]).then(response => {
+        const engineer = new Engineer (response.engineerName, response.engineerId, response.engineerEmail, response.engineerDept, response.engineerGit);
+        all.push(engineer);
+        addMembers();
+    })
+}
+
+function createIntern (){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message:"What is this intern's id?"
+        },
+        {
+            type:"input",
+            name: "internEmail",
+            message: "What is this intern's email address?"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Which school does this intern attend?"
+        }
+    ]).then(response => {
+        const intern = new Intern (response.internName, response.internId, response.internEmail, response.internDept);
+        all.push(intern);
+        addMembers();
+    });
+}
+
+function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(all), "utf-8");
+  }
+
+  addManager();
+
 
 
 
